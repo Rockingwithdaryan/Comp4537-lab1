a8580efcbd9cec36d4068e0a3578e144231704ab
daryanthedev <daryanarpak05@gmail.com>
2026-09-13 19:33:01 -0700

    built the skeleton and installed node and server is ready

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 3000;
+
+const mimeTypes = {
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".css": "text/css",
+  ".json": "application/json", 
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = req.url === "/" ? "/index.html" : req.url;
+  filePath = path.join(__dirname, filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+      return;
+    }
+    const ext = path.extname(filePath);
+    res.writeHead(200, { "Content-Type": mimeTypes[ext] || "application/octet-stream" });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
